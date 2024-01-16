ef37d086ac70b0d418a56acb30f8e8ddd7952c48
Naël EMBARKI <nael.embarki34@gmail.com>
2024-01-16 16:30:03 +0100

    Correction des mes conneries

--- game/code/GameScenes.cc
+++ game/code/GameScenes.cc
@@ -1,12 +1,19 @@
 #include "GameScenes.h"
 #include "constants.h"
+
+//gf::Font font("../../ressources/font/ActiveSpaceDemoRegular.ttf");
+
 namespace swiftness {
-    GameManager::GameManager() 
+    /*GameManager::GameManager() 
     : gf::Scene(gf::Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT))
     , up("Up")
     , down("Down")
     , trigger("Trigger")
     , questionmark("???")
     , quit_a("Quit")
-    , 
+    , quit_b("Quit", font)
+    , credits("Credits", font)
+    , choose_level("Choose level", font)
+    , questionmark_b("???", font)
+    {}*/
 }
