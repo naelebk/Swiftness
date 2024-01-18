71afd434c4a620595ff15104f8e0388ccc1b4b63
typ49 <typ490@gmail.com>
2024-01-18 16:50:10 +0100

    bugfix level 5

--- game/code/levelRender.cc
+++ game/code/levelRender.cc
@@ -1,32 +1,29 @@
 #include "levelRender.h"
 
+namespace swiftness {
 
-namespace swiftness{
-
-    void LevelRender::renderLayer(LayerEntity &layerEntity, gf::RenderWindow &window, LayerName layerName, std::string gravity)
-    {
+    void LevelRender::renderLayer(LayerEntity &layerEntity, gf::RenderWindow &window, LayerName layerName, std::string gravity) {
         gf::TmxTileLayer *layer = layerEntity.getTileLayerByName(layerName);
-        if (layer->visible == true){
-            if ((layerName == LayerName::Gw_blue && gravity == "down") || (layerName == LayerName::Gw_rose && gravity == "up") || (layerName == LayerName::Gw_green && gravity == "left") || (layerName == LayerName::Gw_orange && gravity == "right")){
-                layer->opacity == 0.6;
+        if (layer != nullptr && layer->visible) {
+            if ((layerName == LayerName::Gw_blue && gravity == "down") ||
+                (layerName == LayerName::Gw_rose && gravity == "up") ||
+                (layerName == LayerName::Gw_green && gravity == "left") ||
+                (layerName == LayerName::Gw_orange && gravity == "right")) {
+                    layer->opacity = 0;
             }
 
-            gf::TmxRenderOrder::LeftUp;
+            // Assurez-vous que cette méthode reflète les changements de l'opacité
             gf::TileLayer tileLayer = gf::makeTileLayer(layerEntity.getLayers(), *layer, layerEntity.getResources());
             window.draw(tileLayer);
         }
-
     }
 
-    void LevelRender::renderLevel(std::string nameFile, gf::RenderWindow &window, std::string gravity)
-    {
+    void LevelRender::renderLevel(std::string nameFile, gf::RenderWindow &window, std::string gravity) {
         swiftness::LayerEntity layerEntity(nameFile);
 
-        for (int i = 0 ; i < swiftness::NB_TILE_LAYERS ; i++) {
+        for (int i = 0; i < swiftness::NB_TILE_LAYERS; i++) {
             swiftness::LayerName layerName = static_cast<swiftness::LayerName>(i);
             renderLayer(layerEntity, window, layerName, gravity);
         }
-        
-
     }
 }
