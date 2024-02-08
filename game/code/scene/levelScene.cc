d71d8a46f31ab06d7680cadad603ca6d3e4f4410
typ49 <typ490@gmail.com>
2024-02-08 15:20:42 +0100

    renommage des fichier tmx (plus de problème avec le niveau 10

--- game/code/scene/levelScene.cc
+++ game/code/scene/levelScene.cc
@@ -5,39 +5,17 @@
 #include <iostream>
 #include <gf/Sprite.h>
 
-namespace swiftness {
+namespace swiftness
+{
     // Déclaration en seconde instance du constructeur et du destructeur,
     // de levelScene (interface graphique)
-    levelScene::levelScene(GameCenter& game, gf::Font& font, int level, bool custom) 
-    : gf::Scene(gf::Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT))
-    , m_font(font)
-    , game(game)
-    , m_custom(custom)
-    , m_levelNumber(level)
-    // , m_level.getLevelData()("custom/custom0" + std::to_string(m_levelNumber) + ".tmx")
-    // , m_level.getLevelData()("level0" + std::to_string(m_levelNumber) + ".tmx")
-    , m_level(game.resources, (custom ? "custom/custom0" : "level0") + std::to_string(m_levelNumber) + ".tmx")
-    , up("up")
-    , down("down")
-    , left("left")
-    , right("right")
-    , jump("jump")
-    , upJump("upJump")
-    , downJump("downJump")
-    , leftJump("leftJump")
-    , rightJump("rightJump")
-    , Pause("pause")
-    , map_width(m_level.getLevelData().getMapSize().x)
-    , map_height(m_level.getLevelData().getMapSize().y)
-    , tile_width(m_level.getLevelData().getTileSize().x)
-    , tile_height(m_level.getLevelData().getTileSize().y)
-    , xcamera(m_level.getSquareEntity().getPosition().x)
-    , ycamera(m_level.getSquareEntity().getPosition().y)
-    , konami(0)
-    , konami2(0)
-    , canFly(false)
-    , commandsChange(false)
-     {
+    levelScene::levelScene(GameCenter &game, gf::Font &font, int level, bool custom)
+        : gf::Scene(gf::Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT)), m_font(font), game(game), m_custom(custom), m_levelNumber(level)
+          // , m_level.getLevelData()("custom/custom_" + std::to_string(m_levelNumber) + ".tmx")
+          // , m_level.getLevelData()("level_" + std::to_string(m_levelNumber) + ".tmx")
+          ,
+          m_level(game.resources, (custom ? "custom/custom_" : "level_") + std::to_string(m_levelNumber) + ".tmx"), up("up"), down("down"), left("left"), right("right"), jump("jump"), upJump("upJump"), downJump("downJump"), leftJump("leftJump"), rightJump("rightJump"), Pause("pause"), map_width(m_level.getLevelData().getMapSize().x), map_height(m_level.getLevelData().getMapSize().y), tile_width(m_level.getLevelData().getTileSize().x), tile_height(m_level.getLevelData().getTileSize().y), xcamera(m_level.getSquareEntity().getPosition().x), ycamera(m_level.getSquareEntity().getPosition().y), konami(0), konami2(0), canFly(false), commandsChange(false)
+    {
         gf::Gamepad::initialize();
 
         up.setContinuous();
@@ -46,7 +24,7 @@ namespace swiftness {
         up.addScancodeKeyControl(gf::Scancode::W);
         up.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftY, gf::GamepadAxisDirection::Negative);
         addAction(up);
-        
+
         Pause.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::Start);
         Pause.addScancodeKeyControl(gf::Scancode::P);
         Pause.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::B);
@@ -56,9 +34,8 @@ namespace swiftness {
         upJump.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::DPadUp);
         upJump.addScancodeKeyControl(gf::Scancode::Up);
         upJump.addScancodeKeyControl(gf::Scancode::W);
-        //upJump.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftY, gf::GamepadAxisDirection::Negative);
+        // upJump.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftY, gf::GamepadAxisDirection::Negative);
         addAction(upJump);
-
 
         down.setContinuous();
         down.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::DPadDown);
@@ -82,7 +59,7 @@ namespace swiftness {
         leftJump.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::DPadLeft);
         leftJump.addScancodeKeyControl(gf::Scancode::Left);
         leftJump.addScancodeKeyControl(gf::Scancode::A);
-        //leftJump.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftX, gf::GamepadAxisDirection::Negative);
+        // leftJump.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftX, gf::GamepadAxisDirection::Negative);
         addAction(leftJump);
 
         right.setContinuous();
@@ -95,7 +72,7 @@ namespace swiftness {
         rightJump.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::DPadRight);
         rightJump.addScancodeKeyControl(gf::Scancode::Right);
         rightJump.addScancodeKeyControl(gf::Scancode::D);
-        //rightJump.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftX, gf::GamepadAxisDirection::Positive);
+        // rightJump.addGamepadAxisControl(gf::AnyGamepad, gf::GamepadAxis::LeftX, gf::GamepadAxisDirection::Positive);
         addAction(rightJump);
 
         jump.addGamepadButtonControl(gf::AnyGamepad, gf::GamepadButton::A);
@@ -103,172 +80,232 @@ namespace swiftness {
         addAction(jump);
     }
 
-    int levelScene::getLevel(){ return m_levelNumber; }
+    int levelScene::getLevel() { return m_levelNumber; }
 
-    void levelScene::updateLevel(int s_level) {
+    void levelScene::updateLevel(int s_level)
+    {
         m_levelNumber = s_level;
     }
 
     // Comme on ne peut pas mettre une valeur non comprise entre MIN_LEVEL et MAX_LEVEL, on n'effectue aucune
     // vérification sur la valeur de level, car auquel cas rien ne sera fait
-    void levelScene::loadLevel(int t_level) {
-        if (t_level < 0) {
+    void levelScene::loadLevel(int t_level)
+    {
+        if (t_level < 0)
+        {
             exit(0);
         }
         updateLevel(t_level);
         std::string lvl = "";
-        if (t_level >= 0 && t_level < 10) {
-            lvl = "level0" + std::to_string(t_level) + ".tmx";
+        if (t_level >= 0 && t_level < 10)
+        {
+            lvl = "level_" + std::to_string(t_level) + ".tmx";
             std::cout << GREEN << "Level -> 0" << std::to_string(t_level) << NC << std::endl;
-        } else {
+        }
+        else
+        {
             lvl = "level" + std::to_string(t_level) + ".tmx";
             std::cout << GREEN << "Level -> " << std::to_string(t_level) << NC << std::endl;
         }
-        
-        
-        
+
         m_level.getSquareEntity().setPlateforms(m_level.getPlateforms());
     }
 
-    void levelScene::customLoadLevel(int t_level) {
-        if (t_level < 0) {
+    void levelScene::customLoadLevel(int t_level)
+    {
+        if (t_level < 0)
+        {
             exit(0);
         }
         updateLevel(t_level);
         std::string lvl = "";
-        if (t_level >= 0 && t_level < 10) {
-            lvl = "custom/custom0" + std::to_string(t_level) + ".tmx";
+        if (t_level >= 0 && t_level < 10)
+        {
+            lvl = "custom/custom_" + std::to_string(t_level) + ".tmx";
             std::cout << GREEN << "Level -> 0" << std::to_string(t_level) << NC << std::endl;
-        } else {
+        }
+        else
+        {
             lvl = "custom/custom" + std::to_string(t_level) + ".tmx";
             std::cout << GREEN << "Level -> " << std::to_string(t_level) << NC << std::endl;
         }
-        
+
         m_level.getSquareEntity().setPlateforms(m_level.getPlateforms());
     }
 
-    void levelScene::doHandleActions(gf::Window& window) {
-        if (!isActive()) pause();
-        if (Pause.isActive()) {
+    void levelScene::doHandleActions(gf::Window &window)
+    {
+        if (!isActive())
+            pause();
+        if (Pause.isActive())
+        {
             isPaused() ? resume() : pause();
             game.pushScene(game.s_pause);
             return;
         }
-        if (!commandsChange) {
-            if(up.isActive()) {
+        if (!commandsChange)
+        {
+            if (up.isActive())
+            {
                 m_level.getSquareEntity().actionUp();
-            } else {
+            }
+            else
+            {
                 m_level.getSquareEntity().actionUpRelease();
             }
-            if(down.isActive()){
+            if (down.isActive())
+            {
                 m_level.getSquareEntity().actionDown();
-            } else {
+            }
+            else
+            {
                 m_level.getSquareEntity().actionDownRelease();
             }
-            if(left.isActive()){
+            if (left.isActive())
+            {
                 m_level.getSquareEntity().actionLeft();
-            } else {
+            }
+            else
+            {
                 m_level.getSquareEntity().actionLeftRelease();
             }
-            if (right.isActive()) {
+            if (right.isActive())
+            {
                 m_level.getSquareEntity().actionRight();
-            } else {
+            }
+            else
+            {
                 m_level.getSquareEntity().actionRightRelease();
             }
-            if (upJump.isActive()) {
+            if (upJump.isActive())
+            {
                 m_level.getSquareEntity().actionUpJump();
-                if (konami == 0) {
+                if (konami == 0)
+                {
                     konami++;
-                } else if (konami == 1) {
+                }
+                else if (konami == 1)
+                {
                     konami++;
-                } else if (konami == 2) {
-                    konami=2;
-                } 
-                else {
+                }
+                else if (konami == 2)
+                {
+                    konami = 2;
+                }
+                else
+                {
                     konami = 1;
                 }
             }
-            if (downJump.isActive()){
-                if (konami == 2) {
+            if (downJump.isActive())
+            {
+                if (konami == 2)
+                {
                     konami++;
-                } else if (konami == 3) {
+                }
+                else if (konami == 3)
+                {
                     konami++;
-                } else {
+                }
+                else
+                {
                     konami = 0;
                 }
             }
-            if (leftJump.isActive()) {
+            if (leftJump.isActive())
+            {
                 m_level.getSquareEntity().actionLeftJump();
-                if (konami == 4) {
+                if (konami == 4)
+                {
                     konami++;
-                } else if (konami == 6) {
+                }
+                else if (konami == 6)
+                {
                     konami++;
-                } else {
+                }
+                else
+                {
                     konami = 0;
                 }
             }
-            if (rightJump.isActive()) {
+            if (rightJump.isActive())
+            {
                 m_level.getSquareEntity().actionRightJump();
-                if (konami == 5) {
+                if (konami == 5)
+                {
                     konami++;
-                } else if (konami == 7) {
+                }
+                else if (konami == 7)
+                {
                     konami++;
-                } else {
+                }
+                else
+                {
                     konami = 0;
                 }
             }
-            if (jump.isActive()) {
+            if (jump.isActive())
+            {
                 m_level.getSquareEntity().actionJump();
-                if (konami == 8) {
+                if (konami == 8)
+                {
                     konami++;
-                } else if (konami == 9) {
+                }
+                else if (konami == 9)
+                {
                     konami++;
-                } else {
+                }
+                else
+                {
                     konami = 0;
                 }
             }
-        } else {
+        }
+        else
+        {
             up.isActive() ? m_level.getSquareEntity().actionDown() : m_level.getSquareEntity().actionDownRelease();
             right.isActive() ? m_level.getSquareEntity().actionUp() : m_level.getSquareEntity().actionUpRelease();
             down.isActive() ? m_level.getSquareEntity().actionLeft() : m_level.getSquareEntity().actionLeftRelease();
             left.isActive() ? m_level.getSquareEntity().actionRight() : m_level.getSquareEntity().actionRightRelease();
         }
-        if (konami == 10) {
+        if (konami == 10)
+        {
             m_level.getSquareEntity().setIsFlying(true);
             canFly = true;
-            commandsChange = true; 
+            commandsChange = true;
             konami = -1;
         }
     }
 
-    void levelScene::doRender (gf::RenderTarget& target, const gf::RenderStates &states) {
+    void levelScene::doRender(gf::RenderTarget &target, const gf::RenderStates &states)
+    {
         gf::ExtendView cam(m_camera, {SCREEN_WIDTH, SCREEN_HEIGHT});
-        isPaused() ? target.clear(gf::Color::Blue) :
-        canFly && commandsChange ? target.clear(gf::Color::Violet/*a*/) :
-        target.clear(gf::Color::fromRgb(0.2, 0.2, 0.2));
+        isPaused() ? target.clear(gf::Color::Blue) : canFly &&commandsChange ? target.clear(gf::Color::Violet /*a*/)
+                                                                             : target.clear(gf::Color::fromRgb(0.2, 0.2, 0.2));
         target.setView(cam);
         swiftness::LevelRender renderLevel;
         renderLevel.renderLevel(game.resources, m_level.getLevelData(), target, m_level.getSquareEntity().getGravity());
-        // std::cout << "level name : " << (m_custom ? "custom0" : "level0") + std::to_string(m_levelNumber) + ".tmx" << std::endl; 
         m_level.getSquareEntity().render(target, states);
         m_level.getSquareEntity().renderHUD(target, SCREEN_WIDTH, SCREEN_HEIGHT, m_camera);
     }
 
-    void levelScene::doUpdate(gf::Time time) {
+    void levelScene::doUpdate(gf::Time time)
+    {
         m_level.getSquareEntity().update(time);
-        if (m_level.getSquareEntity().getLevelOver()) {
+        if (m_level.getSquareEntity().getLevelOver())
+        {
             game.levelTheme.stop();
             game.mainTheme.play();
             game.replaceAllScenes(game.menu, trans, gf::milliseconds(500));
         }
-        map_width=m_level.getLevelData().getMapSize().x;
-        map_height=m_level.getLevelData().getMapSize().y;
-        tile_width=m_level.getLevelData().getTileSize().x;
-        tile_height=m_level.getLevelData().getTileSize().y;
-        xcamera=m_level.getSquareEntity().getPosition().x;
-        ycamera=m_level.getSquareEntity().getPosition().y;
-        xcamera=gf::clamp(xcamera,SCREEN_WIDTH/2+tile_width,map_width*tile_width-SCREEN_WIDTH/2-tile_width);
-        ycamera=gf::clamp(ycamera,SCREEN_HEIGHT/2+tile_height,map_height*tile_height-SCREEN_HEIGHT/2-tile_height);
+        map_width = m_level.getLevelData().getMapSize().x;
+        map_height = m_level.getLevelData().getMapSize().y;
+        tile_width = m_level.getLevelData().getTileSize().x;
+        tile_height = m_level.getLevelData().getTileSize().y;
+        xcamera = m_level.getSquareEntity().getPosition().x;
+        ycamera = m_level.getSquareEntity().getPosition().y;
+        xcamera = gf::clamp(xcamera, SCREEN_WIDTH / 2 + tile_width, map_width * tile_width - SCREEN_WIDTH / 2 - tile_width);
+        ycamera = gf::clamp(ycamera, SCREEN_HEIGHT / 2 + tile_height, map_height * tile_height - SCREEN_HEIGHT / 2 - tile_height);
         m_camera = {xcamera, ycamera};
     }
 }
--- game/code/scene/selectLevelScene.cc
+++ game/code/scene/selectLevelScene.cc
@@ -3,32 +3,38 @@
 #include <gf/Coordinates.h>
 #include "levelScene.h"
 
-namespace swiftness {
-    SelectLevel::SelectLevel(GameCenter& game, gf::Font& font) :
-    gf::Scene(gf::Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT)),
-    font(font),
-    game(game),
-    up("Up"),
-    down("Down"),
-    trigger("Trigger"),
-    quit("Quit"),
-    level00(game, font, 0, false),
-    level01(game, font, 1, false),
-    level02(game, font, 2, false),
-    level03(game, font, 3, false),
-    level04(game, font, 4, false),
-    level05(game, font, 5, false),
-    level06(game, font, 6, false),
-    level07(game, font, 7, false),
-    level08(game, font, 8, false),
-    level09(game, font, 9, false)
+namespace swiftness
+{
+    SelectLevel::SelectLevel(GameCenter &game, gf::Font &font) : gf::Scene(gf::Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT)),
+                                                                 font(font),
+                                                                 game(game),
+                                                                 up("Up"),
+                                                                 down("Down"),
+                                                                 trigger("Trigger"),
+                                                                 quit("Quit"),
+                                                                 level00(game, font, 0, false),
+                                                                 level01(game, font, 1, false),
+                                                                 level02(game, font, 2, false),
+                                                                 level03(game, font, 3, false),
+                                                                 level04(game, font, 4, false),
+                                                                 level05(game, font, 5, false),
+                                                                 level06(game, font, 6, false),
+                                                                 level07(game, font, 7, false),
+                                                                 level08(game, font, 8, false),
+                                                                 level09(game, font, 9, false)
     {
-        for (int i = 0 ; i < MAX_LEVEL + 2 ; ++i) {
-            if (i >= 1 && i < MAX_LEVEL + 1) {
+        for (int i = 0; i < MAX_LEVEL + 2; ++i)
+        {
+            if (i >= 1 && i < MAX_LEVEL + 1)
+            {
                 levels_b.emplace_back("Level " + std::to_string(i), font, 60);
-            } else if (i == 0) {
+            }
+            else if (i == 0)
+            {
                 levels_b.emplace_back("Tutorial", font, 60);
-            } else {
+            }
+            else
+            {
                 levels_b.emplace_back("Main Menu", font, 60);
             }
         }
@@ -54,7 +60,8 @@ namespace swiftness {
         trigger.addMouseButtonControl(gf::MouseButton::Left);
         addAction(trigger);
 
-        auto createButtons = [&] (gf::TextButtonWidget& button, auto callback) {
+        auto createButtons = [&](gf::TextButtonWidget &button, auto callback)
+        {
             button.setDefaultTextColor(gf::Color::Black);
             button.setDefaultBackgroundColor(gf::Color::Gray(0.7f));
             button.setSelectedTextColor(gf::Color::Black);
@@ -66,167 +73,154 @@ namespace swiftness {
             button.setCallback(callback);
             widgets.addWidget(button);
         };
-        createButtons(levels_b[MAX_LEVEL + 1], [&] () {
-            game.replaceAllScenes(game.menu, trans, gf::milliseconds(500));
-        });
-
-        std::cout << "Depuis selectLevelScene\n";
+        createButtons(levels_b[MAX_LEVEL + 1], [&]()
+                      { game.replaceAllScenes(game.menu, trans, gf::milliseconds(500)); });
         level00.loadLevel(0);
 
-        createButtons(levels_b[0], [&] () {
-            game.mainTheme.stop();
-            game.levelTheme.play();
-            game.replaceAllScenes(level00, pixel, gf::milliseconds(500));
-            
-        });
+        createButtons(levels_b[0], [&]()
+                      {
+                          game.mainTheme.stop();
+                          game.levelTheme.play();
+                          game.replaceAllScenes(level00, pixel, gf::milliseconds(500));
+                      });
 
-        std::cout << "Depuis selectLevelScene\n";
         level01.loadLevel(1);
 
-        createButtons(levels_b[1], [&] () {
+        createButtons(levels_b[1], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level01, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level01, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level02.loadLevel(2);
-        
-        createButtons(levels_b[2], [&] () {
+
+        createButtons(levels_b[2], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level02, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level02, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level03.loadLevel(3);
 
-        createButtons(levels_b[3], [&] () {
+        createButtons(levels_b[3], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level03, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level03, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level04.loadLevel(4);
 
-        createButtons(levels_b[4], [&] () {
+        createButtons(levels_b[4], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level04, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level04, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level05.loadLevel(5);
 
-        createButtons(levels_b[5], [&] () {
+        createButtons(levels_b[5], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level05, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level05, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level06.loadLevel(6);
 
-        createButtons(levels_b[6], [&] () {
+        createButtons(levels_b[6], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level06, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level06, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level07.loadLevel(7);
 
-        createButtons(levels_b[7], [&] () {
+        createButtons(levels_b[7], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level07, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level07, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level08.loadLevel(8);
 
-        createButtons(levels_b[8], [&] () {
+        createButtons(levels_b[8], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level08, pixel, gf::milliseconds(500));
-        });
+            game.replaceAllScenes(level08, pixel, gf::milliseconds(500)); });
 
-
-        std::cout << "Depuis selectLevelScene\n";
         level09.loadLevel(9);
 
-        createButtons(levels_b[9], [&] () {
+        createButtons(levels_b[9], [&]()
+                      {
             game.mainTheme.stop();
             game.levelTheme.play();
-            game.replaceAllScenes(level09, pixel, gf::milliseconds(500));
-        });
-
-        /*createButtons(levels_b[10], [&] () {
-            game.replaceAllScenes(level10, pixel, gf::milliseconds(500));
-        });*/
-
-
+            game.replaceAllScenes(level09, pixel, gf::milliseconds(500)); });
     }
 
     SelectLevel::~SelectLevel() {}
 
-    gf::WidgetContainer SelectLevel::GameLevel_getWidgets() {
+    gf::WidgetContainer SelectLevel::GameLevel_getWidgets()
+    {
         return widgets;
     }
 
-    std::string getLevelNameByButton(gf::TextButtonWidget& button) {
+    std::string getLevelNameByButton(gf::TextButtonWidget &button)
+    {
         std::string buttonName = button.getString();
         return buttonName == "Tutorial" ? "level00.tmx" : buttonName + ".tmx";
     }
 
     // Méthodes virtuelles privées héritant directement de gf::Scene
-    void SelectLevel::doHandleActions(gf::Window& window) {
-        if (!window.isOpen()) return;
-        if (up.isActive()) widgets.selectPreviousWidget();
-        if (down.isActive()) widgets.selectNextWidget();
-        if (trigger.isActive()) widgets.triggerAction();
-        if (quit.isActive()) game.replaceScene(game.menu, trans, gf::milliseconds(500));
+    void SelectLevel::doHandleActions(gf::Window &window)
+    {
+        if (!window.isOpen())
+            return;
+        if (up.isActive())
+            widgets.selectPreviousWidget();
+        if (down.isActive())
+            widgets.selectNextWidget();
+        if (trigger.isActive())
+            widgets.triggerAction();
+        if (quit.isActive())
+            game.replaceScene(game.menu, trans, gf::milliseconds(500));
     }
 
-    void SelectLevel::doProcessEvent(gf::Event& event) {
-        switch(event.type) {
-            case gf::EventType::MouseMoved:
-                widgets.pointTo(game.computeWindowToGameCoordinates(event.mouseCursor.coords, getHudView()));
-                break;
-            default:
-                break;
+    void SelectLevel::doProcessEvent(gf::Event &event)
+    {
+        switch (event.type)
+        {
+        case gf::EventType::MouseMoved:
+            widgets.pointTo(game.computeWindowToGameCoordinates(event.mouseCursor.coords, getHudView()));
+            break;
+        default:
+            break;
         }
     }
 
-    void SelectLevel::doRender(gf::RenderTarget& target, const gf::RenderStates &states) {
+    void SelectLevel::doRender(gf::RenderTarget &target, const gf::RenderStates &states)
+    {
         float size = 0.025f, space = 0.03f;
-        gf::Vector2f bg_size (0.55f, 0.1f); 
+        gf::Vector2f bg_size(0.55f, 0.1f);
         target.setView(getHudView());
         gf::Coordinates coords(target);
         float width = coords.getRelativeSize(bg_size - 0.05f).x, padding = coords.getRelativeSize({0.01f, 0.f}).x;
         int r_size = coords.getRelativeCharacterSize(size);
         float ne = 0.0f;
-        for (int i = 0 ; i < MAX_LEVEL + 2 ; ++i) {
-            ne = 0.05f + (size + space + 0.03f)*i;
+        for (int i = 0; i < MAX_LEVEL + 2; ++i)
+        {
+            ne = 0.05f + (size + space + 0.03f) * i;
             levels_b[i].setCharacterSize(r_size);
             levels_b[i].setPosition(coords.getRelativePoint({0.275f, ne}));
             levels_b[i].setParagraphWidth(width);
             levels_b[i].setPadding(padding);
-        }        
+        }
         widgets.render(target, states);
-
     }
-    void SelectLevel::doShow() {
+    void SelectLevel::doShow()
+    {
         widgets.clear();
-        for (gf::TextButtonWidget& b : levels_b) {
+        for (gf::TextButtonWidget &b : levels_b)
+        {
             b.setDefault();
             widgets.addWidget(b);
         }
